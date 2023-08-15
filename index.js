aaec2e0b98ca3ca3e85185b9cfa6d673dc620c2d
bilolov <elyorbilolovelyorbilolov@gmail.com>
2023-08-15 16:25:57 +0500

    111

---
+++ index.js
@@ -0,0 +1,8 @@
+/* jshint esversion: 6 */
+//index.js
+
+const User = require("./users");
+
+const elyor = new User("Elyor", 24);
+
+console.log(elyor.hello());
---
+++ users.js
@@ -0,0 +1,16 @@
+/* jshint esversion: 6 */
+//users.js
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    hello() {
+        console.log(
+            `Hello my name is ${this.name} and I'm ${this.age} years old`
+        );
+    }
+}
+
+module.exports = User;
